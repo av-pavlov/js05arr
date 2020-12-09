9d9a1a566340eb1149824622a674ea075352e0f3
Александр Павлов <7911562+av-pavlov@users.noreply.github.com>
2020-12-09 13:27:14 +0900

    init

--- run-xx.js
+++ run-xx.js
@@ -1,5 +1,6 @@
-// это файл с проверками
-const f = require('./arr-x1.js');
+// это файл с проверками для задачи x.x
+// запускать так: node run-xx.js
+const f = require('./arr-xx.js');
 const assert = require('assert');
 
 assert.deepStrictEqual(f(1), 100, "ОШИБКА! НЕВЕРНО ДЛЯ f(1)");
